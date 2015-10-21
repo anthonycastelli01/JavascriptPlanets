ec7a7c66a8f9694e21312a87571388e77422ee23
Anthony Castelli <anthonycastelli01@gmail.com>
2015-10-21 09:11:00 -0700

    Update formatting to current knowldege of web design

--- public/js/space_generator.js
+++ public/js/space_generator.js
@@ -1,10 +1,10 @@
-var seed 			= 40,
-	map_width 		= 60,
-	map_height 		= 40,
-	star_density 	= 0.09;
-	
+var seed 					= 40,
+		map_width 		= 60,
+		map_height 		= 40,
+		star_density 	= 0.09;
+
 var minPlanets 		= 1,
-	maxPlanets 		= 3;
+		maxPlanets 		= 3;
 
 init();
 
@@ -16,11 +16,11 @@ function init() {
 
 function create_stars(seed, width, height, density) {
 	var output_values = [];
-	
+
 	for (var y = 0; y < height; y++) {
 		output_values[y] = new Array(width);
 	}
-	
+
 	for (var y = 0; y < height; y++) {
 		for (var x = 0; x < width; x++) {
 			if (Math.random() < density) {
@@ -33,17 +33,17 @@ function create_stars(seed, width, height, density) {
 			}
 		}
 	}
-	
+
 	return output_values
 }
 
 function create_planets(star_field) {
 	var planet_map = [];
-	
+
 	for (var y = 0; y < star_field.length; y++) {
 		planet_map[y] = new Array(star_field[0].length);
 	}
-	
+
 	for (var y = 0; y < planet_map.length; y++) {
 		for (var x = 0; x < planet_map[0].length; x++) {
 			if (star_field[y][x] > 0) {
@@ -51,21 +51,21 @@ function create_planets(star_field) {
 			}
 		}
 	}
-	
+
 	return planet_map
 }
 
 function system_generator(seed, min, max, x, y) {
 	var planet_seeds = [];
 	var numPlanets = Math.floor((Math.random()*max)+min);
-	
+
 	console.log("Number of planets at location", x, ",", y, " is: ", numPlanets);
-	
+
 	for (var i = 0; i < numPlanets; i++) {
 		planet_seeds[i] = Math.random();
 		console.log("Planet ", i, " has a seed of ", planet_seeds[i]);
 	}
-	
+
 	return planet_seeds
 }
 
@@ -73,11 +73,9 @@ function visual_output(array_in) {
 	for (var y = 0; y < array_in.length; y++) {
 		for (var x = 0; x < array_in[0].length; x++) {
 			if (array_in[y][x] > 0) {
-				document.write("O ");
-			}
-			else
-			{
-				document.write("<span id=\"void\">- </span>");
+				document.write('<span class="star">O </span>');
+			} else {
+				document.write('<span class="void">- </span>');
 			}
 		}
 		document.write("<br>");

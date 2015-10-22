d97ec1aa4ef6bbb4f6604239feab436f2807cb57
Anthony Castelli <anthonycastelli01@gmail.com>
2015-10-22 15:09:15 -0700

    Implement distribution of systems normally around center of x-axis

--- public/js/space_generator.js
+++ public/js/space_generator.js
@@ -1,7 +1,7 @@
 // Galaxy constants
 var SEED 					= 40,
 		MAP_WIDTH 		= 50,
-		MAP_HEIGHT 		= 30,
+		MAP_HEIGHT 		= 50,
 		STAR_DENSITY 	= 0.09;
 
 // System ranges
@@ -13,7 +13,8 @@ init();
 function init() {
 	// Generate a field of seed values dispersed based on the density
 	//  and size of the region of space
-	var starField = generateSeeds(SEED, MAP_WIDTH, MAP_HEIGHT, STAR_DENSITY);
+	// var starField = generateSeeds(SEED, MAP_WIDTH, MAP_HEIGHT, STAR_DENSITY);
+	var starField = generateLinearlyWeightedSeeds(SEED, MAP_WIDTH, MAP_HEIGHT, STAR_DENSITY);
 	// Display field on the page
 	displayGalaxy(starField);
 	// Generate planets for systems based on seed input
@@ -35,7 +36,37 @@ function generateSeeds(seed, width, height, density) {
 			if (Math.random() < density) {
 				// If below density threshold add a seed value into the array
 				seedMap[y][x] = Math.random();
-				console.log("Star generated at ", x, " ", y, " with seed ", seedMap[y][x]);
+				// console.log("Star generated at ", x, " ", y, " with seed ", seedMap[y][x]);
+			} else {
+				// Otherwise the system has no value
+				seedMap[y][x] = 0;
+			}
+		}
+	}
+
+	return seedMap
+}
+
+function generateLinearlyWeightedSeeds(seed, width, height, density) {
+	var seedMap = [];
+	var weightedX = MAP_WIDTH / 2; // For a vertical weight
+
+	// Generate an array to hold the stars
+	for (var y = 0; y < height; y++) {
+		seedMap[y] = new Array(width);
+	}
+
+	// Loop to fill the new empty array with stars
+	for (var y = 0; y < height; y++) {
+		for (var x = 0; x < width; x++) {
+			// Compute the probability of a star existing here
+			var probability = Math.exp(-(Math.pow(x - (MAP_WIDTH/2),2)/(MAP_WIDTH)));
+			console.log(probability);
+
+			if (Math.random() < probability) {
+				// If below density threshold add a seed value into the array
+				seedMap[y][x] = Math.random();
+				// console.log("Star generated at ", x, " ", y, " with seed ", seedMap[y][x]);
 			} else {
 				// Otherwise the system has no value
 				seedMap[y][x] = 0;
@@ -73,12 +104,12 @@ function systemGenerator(seed, min, max, x, y) {
 	// Give the system a number of planets
 	var numPlanets = Math.floor((Math.random()*max)+min);
 
-	console.log("Number of planets at location", x, ",", y, " is: ", numPlanets);
+	// console.log("Number of planets at location", x, ",", y, " is: ", numPlanets);
 
 	// Give each planet a seed value
 	for (var i = 0; i < numPlanets; i++) {
 		planetSeeds[i] = Math.random();
-		console.log("Planet ", i, " has a seed of ", planetSeeds[i]);
+		// console.log("Planet ", i, " has a seed of ", planetSeeds[i]);
 	}
 
 	return planetSeeds
